86b55d9930930963543c335d9f5e1143da92be52
jokley <reinhard_juen@vol.at>
2021-09-20 15:49:33 +0200

    Update dashboard.js

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -15,55 +15,56 @@ const useStyles = makeStyles({
   },
 });
 
-const rnd = () => Math.floor(Math.random() * 20) + 1;
+
+const TimestampNow = Math.round(new Date().getTime()/1000);
+
 
 export default function App() {
-
-  const url = ('http://127.0.0.1:5000/senors/1');
- 
-
+  
+  
+  
   const classes = useStyles();
   
-  const [data1, setData1] = useState([1, 2, 3, 4, 5, 6]);
-  const [data2, setData2] = useState([1, 2, 3, 4, 5, 6]);
-  const [data3, setData3] = useState([1, 2, 3, 4, 5, 6]);
-  const [labels,setLabels]= useState(["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]);
- 
-  const handleData = () => {
-    setData1([rnd(), rnd(), rnd(), rnd(), rnd(), rnd()]);
-    setData2([rnd(), rnd(), rnd(), rnd(), rnd(), rnd()]);
-    setData3([rnd(), rnd(), rnd(), rnd(), rnd(), rnd()]);
-    
+  const [to,setTo] = useState(TimestampNow);
+  const [from, setFrom] = useState(TimestampNow-3600);
 
-    setLabels(["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]);
+
+  const handleData = () => {
+    setFrom(TimestampNow-36000)
+
   };
+  const url = (`http://127.0.0.1:5000/senors?from=${from}&to=${to}`);
 
   const { data, error } = useSWR(url);
 
   if (error) return <h1>Something went wrong!</h1>
   if (!data) return <h1>Loading...</h1>
 
+  const SensorDataBoilerTop = data.filter((item)=> item.name === 'Boiler Top').map((item)=> item.temp)
+  const SensorDataCoolerIn = data.filter((item)=> item.name === 'Cooler In').map((item)=> item.temp)
+  const SensorDataCoolerOut = data.filter((item)=> item.name === 'Cooler Out').map((item)=> item.temp)
+  const SensorLabel = data.filter((item)=> item.name === 'Boiler Top').map((item)=> new Date (item.date).toLocaleTimeString());
+  
+  
   return (
     <div className="App">
-     
+      
       <Button className={classes.root} onClick={handleData}> Data Random</Button>
       <Grid  container
-  direction="row"
-  justifyContent="space-around"
-  alignItems="center">
+        direction="row"
+        justifyContent="space-around"
+        alignItems="center">
       <Card  >
-      <ExampleChart data={data1} labels={labels} options={options} />
+      <ExampleChart label ='Boiler Top' data={SensorDataBoilerTop} labels={SensorLabel} options={options} />
       </Card>
       <Card >
-      <ExampleChart data={data2} labels={labels} options={options} />
+      <ExampleChart label ='Cooler In' data={SensorDataCoolerIn} labels={SensorLabel} options={options} />
       </Card>
       <Card >
-      <ExampleChart data={data3} labels={labels} options={options} />
+      <ExampleChart label ='Cooler Out' data={SensorDataCoolerOut} labels={SensorLabel} options={options} />
       </Card>
       </Grid>
-      
-      
-
+ 
     </div>
   );
 }
