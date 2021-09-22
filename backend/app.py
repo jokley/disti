80b707aead390eaad7eb9b159363d40f57d18870
jokley <reinhard_juen@vol.at>
2021-09-22 15:59:13 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -264,7 +264,5 @@ def handle_car(car_id):
         db.session.commit()
         return {"message": f"Car {car.name} successfully deleted."}
     
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
 
 db.create_all()
