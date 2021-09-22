05b4cdf44b11ae14c803cb4f3e0a2a5a171c257e
jokley <reinhard_juen@vol.at>
2021-09-22 17:03:46 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -101,15 +101,15 @@ mqtt.subscribe("/sensors")
     
 @mqtt.on_message()
 def handle_message(client, userdata, message):
-   if message.topic == "/sensors":
+   #if message.topic == "/sensors":
        data = json.loads(message.payload)
-       if data['type'] == "ds18b20":
-           new_sensor =  Sensor(name=data['name'],type=data['type'], temp=data['temp']) 
-       elif data['type'] == "si7021":
-           new_sensor =  Sensor(name=data['name'],type=data['type'],temp=data['temp'],humi=data['humi'])   
+       #if data['type'] == "ds18b20":
+           #new_sensor =  Sensor(name=data['name'],type=data['type'], temp=data['temp']) 
+       #elif data['type'] == "si7021":
+           #new_sensor =  Sensor(name=data['name'],type=data['type'],temp=data['temp'],humi=data['humi'])   
        print(data)
-       db.session.add(new_sensor)
-       db.session.commit()
+       #db.session.add(new_sensor)
+       #db.session.commit()
         
         
 @mqtt.on_connect()
