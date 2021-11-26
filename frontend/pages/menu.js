dcb228008ae8992e4de01cb39acd56ea81ea4ee3
jokley <reinhard_juen@vol.at>
2021-11-26 07:53:02 +0100

    Update menu.js

--- frontend/pages/menu.js
+++ frontend/pages/menu.js
@@ -1,17 +1,9 @@
 import * as React from "react";
-import { useSession } from "next-auth/client"; 
+
 
 const MenuPage = () => {
-  const [session, loading]= useSession()
-
-  if (!session) {
-    return (
-    <p>
-    <a href="/api/auth/signin">Sign in</a>
-    </p>)
-  }
- 
-  return <h1>{session ? `${session.user.name}, `: ''}This is menu page</h1>;
+  
+  return <h1>This is menu page</h1>;
 };
 
 export default MenuPage;
