b2b2043615e959af272c7a61020fbe4b43ae8536
jokley <reinhard_juen@vol.at>
2021-11-26 08:40:04 +0100

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,4 @@
-import { Provider } from 'next-auth/client';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import createEmotionCache from '../styles/createEmotionCache';
-
-import "../styles/globals.css";
-import { SWRConfig } from 'swr'
-import theme from '../styles/theme';
-import Header from "@components/Header";
-
-
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
-
-const fetcher = (...args) => fetch(...args).then((res) => res.json())import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
@@ -49,29 +33,5 @@ export default function MyApp(props) {
       </ThemeProvider>
       </SWRConfig>
     </CacheProvider>
-  );
-}
-
-
-export default function MyApp(props) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
-  return (
-    <Provider>
-    <CacheProvider value={emotionCache}>
-       <SWRConfig value={{ fetcher }}>
-      <Head>
-        <title>planty</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Header />
-        <Component {...pageProps} />
-      </ThemeProvider>
-      </SWRConfig>
-    </CacheProvider>
-    </Provider>
   );
 }
