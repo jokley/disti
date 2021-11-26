fdc7b9ed4fb66d4d5c8b494eb4dbf2268cae98fc
jokley <reinhard_juen@vol.at>
2021-11-26 07:53:50 +0100

    Update [...nextauth].js

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+// import NextAuth from "next-auth";
+// import GitHubProvider from 'next-auth/providers/github';
 
-export default NextAuth ({
-    providers: [
-        Providers.GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-    ],
+// export default NextAuth ({
+//     providers: [
+//         GitHubProvider({
+//             clientId: process.env.GITHUB_ID,
+//             clientSecret: process.env.GITHUB_SECRET,
+//         }),
+//     ],
    
-})
+// })
