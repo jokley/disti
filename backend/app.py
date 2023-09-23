b1ad4271147a634f53aed6b9849331d675f4203a
jokley <reinhard_juen@vol.at>
2023-09-23 18:51:27 +0200

    app.py

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_marshmallow import Marshmallow
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from datetime import datetime
 import pytz
+import psycopg2
 from flask_cors import CORS
 from flask_mqtt import Mqtt
 from dotenv import load_dotenv
@@ -34,8 +35,15 @@ app = Flask(__name__)
 CORS(app)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@postgres/flasksql'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+def get_db_connection():
+    conn = psycopg2.connect(host='postgres',
+                            database='flasksql',
+                            user=os.getenv("DOCKER_POSTGRES_INIT_USERNAME"),
+                            password=os.getenv("DOCKER_POSTGRES_INIT_PASSWORD"))
+    return conn
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@postgres/flasksql'
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config['MQTT_BROKER_URL'] = "172.16.238.12"
 app.config['MQTT_BROKER_PORT'] = 1883
@@ -52,71 +60,71 @@ mqtt = Mqtt(app)
 
 
 
-class Sensor(db.Model):
-    id = db.Column(db.Integer)
-    name = db.Column(db.Text(), nullable=False)
-    type = db.Column(db.Text(), nullable=False)
-    temp = db.Column(db.Float(2), nullable=True)
-    humi = db.Column(db.Float(2), nullable=True)
-    date = db.Column(db.DateTime(), nullable=False)
+# class Sensor(db.Model):
+#     id = db.Column(db.Integer)
+#     name = db.Column(db.Text(), nullable=False)
+#     type = db.Column(db.Text(), nullable=False)
+#     temp = db.Column(db.Float(2), nullable=True)
+#     humi = db.Column(db.Float(2), nullable=True)
+#     date = db.Column(db.DateTime(), nullable=False)
 
-    # def __init__(self, name, type,temp,humi=None):
-    #     self.name = name
-    #     self.type = type
-    #     self.temp = temp
-    #     self.humi = humi
+#     # def __init__(self, name, type,temp,humi=None):
+#     #     self.name = name
+#     #     self.type = type
+#     #     self.temp = temp
+#     #     self.humi = humi
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
        
 
-class SensorSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Sensor
-        ordered = True
+# class SensorSchema(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = Sensor
+#         ordered = True
 
-sensor_schema = SensorSchema()
-sensors_schema = SensorSchema(many=True)
+# sensor_schema = SensorSchema()
+# sensors_schema = SensorSchema(many=True)
     
 
-class People(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    pname = db.Column(db.String(80), unique=True, nullable=False)
-    color = db.Column(db.String(120), nullable=False)
+# class People(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     pname = db.Column(db.String(80), unique=True, nullable=False)
+#     color = db.Column(db.String(120), nullable=False)
     
 
-    def __init__(self, pname, color):
-        self.pname = pname
-        self.color = color
+#     def __init__(self, pname, color):
+#         self.pname = pname
+#         self.color = color
 
-class PeopleSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = People
-        ordered = True
+# class PeopleSchema(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = People
+#         ordered = True
 
-people_schema = PeopleSchema()
-poeples_schema = PeopleSchema(many=True)
+# people_schema = PeopleSchema()
+# poeples_schema = PeopleSchema(many=True)
 
-class CarsModel(db.Model):
-    __tablename__ = 'cars'
+# class CarsModel(db.Model):
+#     __tablename__ = 'cars'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    model = db.Column(db.String())
-    doors = db.Column(db.Integer())
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String())
+#     model = db.Column(db.String())
+#     doors = db.Column(db.Integer())
 
-    def __init__(self, name, model, doors):
-        self.name = name
-        self.model = model
-        self.doors = doors
+#     def __init__(self, name, model, doors):
+#         self.name = name
+#         self.model = model
+#         self.doors = doors
 
-class CarSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = CarsModel
-        ordered = True
+# class CarSchema(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = CarsModel
+#         ordered = True
 
-car_schema = CarSchema()
-cars_schema = CarSchema(many=True)
+# car_schema = CarSchema()
+# cars_schema = CarSchema(many=True)
 
 
 mqtt.subscribe("sensors/#")
@@ -186,7 +194,7 @@ def handle_connect(client, userdata, flags, rc):
 
 @app.route('/')
 def home():
-    return '<a href="/addperson"><button> Click here </button></a>'
+    return jsonify('Dist API')
 
 @app.route('/time')
 def time():
@@ -199,140 +207,152 @@ def mqtt_subscribe():
     return jsonify("subscribe topic sensors/#")
 
  
-@app.route("/persons")
-def get_persons():
-    all_people = People.query.all()
-    return jsonify(poeples_schema.dump(all_people))
+# @app.route("/persons")
+# def get_persons():
+#     all_people = People.query.all()
+#     return jsonify(poeples_schema.dump(all_people))
 
 
-@app.route("/addperson")
-def addperson():
-    return render_template("index.html")
+# @app.route("/addperson")
+# def addperson():
+#     return render_template("index.html")
 
 
-@app.route("/personadd", methods=['POST'])
-def personadd():
-    pname = request.form["pname"]
-    color = request.form["color"]
-    entry = People(pname, color)
-    db.session.add(entry)
-    db.session.commit()
+# @app.route("/personadd", methods=['POST'])
+# def personadd():
+#     pname = request.form["pname"]
+#     color = request.form["color"]
+#     entry = People(pname, color)
+#     db.session.add(entry)
+#     db.session.commit()
 
-    return render_template("index.html")
-
-
-@app.route('/sensors', methods=['POST', 'GET'])
-def handle_senors():
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            if data['type'] == "ds18b20":
-                new_sensor =  Sensor(name=data['name'],type=data['type'], temp=data['temp'],date=get_timestamp_now()) 
-            elif data['type'] == "si7021":
-                new_sensor =  Sensor(name=data['name'],type=data['type'],temp=data['temp'],humi=data['humi'],date=get_timestamp_now())   
-
-            db.session.add(new_sensor)
-            db.session.commit()
-            return {"message": f"sensor {new_sensor.name} has been created successfully."}
-        else:
-            return {"error": "The request payload is not in JSON format"}
-
-    elif request.method == 'GET':
-        FROM =request.args.get('from', default = get_timestamp_now_epoche()-36000, type = int)
-        TO = request.args.get('to', default = get_timestamp_now_epoche(), type = int)
-
-        VON = datetime.fromtimestamp(int(FROM + get_timestamp_now_offset())).isoformat()
-        BIS = datetime.fromtimestamp(int(TO + get_timestamp_now_offset())).isoformat()  
-
-        all_senors = Sensor.query.filter(Sensor.date >= VON).filter(Sensor.date<=BIS).order_by(Sensor.date).all()
-    return jsonify(sensors_schema.dump(all_senors))
+#     return render_template("index.html")
 
 
-@app.route('/senors/<senors_id>', methods=['GET', 'PUT', 'DELETE'])
-def handle_sensor(senors_id):
-    sensor = Sensor.query.get_or_404(senors_id)
+# @app.route('/sensors', methods=['POST', 'GET'])
+# def handle_senors():
+#     if request.method == 'POST':
+#         if request.is_json:
+#             data = request.get_json()
+#             if data['type'] == "ds18b20":
+#                 new_sensor =  Sensor(name=data['name'],type=data['type'], temp=data['temp'],date=get_timestamp_now()) 
+#             elif data['type'] == "si7021":
+#                 new_sensor =  Sensor(name=data['name'],type=data['type'],temp=data['temp'],humi=data['humi'],date=get_timestamp_now())   
 
-    if request.method == 'GET':
-        response = {
-            "name": sensor.name,
-            "type": sensor.type,
-            "temp": sensor.temp,
-            "humi": sensor.humi,
-            "date": sensor.date
-        }
-        return {"message": "success", "sensor": response}
+#             db.session.add(new_sensor)
+#             db.session.commit()
+#             return {"message": f"sensor {new_sensor.name} has been created successfully."}
+#         else:
+#             return {"error": "The request payload is not in JSON format"}
 
-    elif request.method == 'PUT':
-        data = request.get_json()
-        if data['type'] == "ds18b20":
-            sensor.name = data['name']
-            sensor.temp = data['temp']
-            sensor.date = get_timestamp_now()
-        elif data['type'] == "si7021":
-            sensor.name = data['name']
-            sensor.temp = data['temp']
-            sensor.humi = data['humi']
-            sensor.date = get_timestamp_now()
+#     elif request.method == 'GET':
+#         FROM =request.args.get('from', default = get_timestamp_now_epoche()-36000, type = int)
+#         TO = request.args.get('to', default = get_timestamp_now_epoche(), type = int)
 
-        db.session.add(sensor)
-        db.session.commit()
-        return {"message": f"sensor {sensor.name} successfully updated"}
+#         VON = datetime.fromtimestamp(int(FROM + get_timestamp_now_offset())).isoformat()
+#         BIS = datetime.fromtimestamp(int(TO + get_timestamp_now_offset())).isoformat()  
 
-    elif request.method == 'DELETE':
-        db.session.delete(sensor)
-        db.session.commit()
-        return {"message": f"sensor {sensor.name} successfully deleted."}
+#         all_senors = Sensor.query.filter(Sensor.date >= VON).filter(Sensor.date<=BIS).order_by(Sensor.date).all()
+#     return jsonify(sensors_schema.dump(all_senors))
 
 
-@app.route('/cars', methods=['POST', 'GET'])
-def handle_cars():
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
-            db.session.add(new_car)
-            db.session.commit()
-            return {"message": f"car {new_car.name} has been created successfully."}
-        else:
-            return {"error": "The request payload is not in JSON format"}
-
-    elif request.method == 'GET':
-        all_cars = CarsModel.query.all()
-        # results = [
-        #     {
-        #         "name": car.name,
-        #         "model": car.model,
-        #         "doors": car.doors
-        #     } for car in cars]
-
-        return jsonify(cars_schema.dump(all_cars))
+@app.route('/senors', methods=['GET'])
+def handle_sensor():
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM sensor;')
+        sensors = cur.fetchall()
+        cur.close()
+        conn.close()
+        return jsonify(sensors)
+    
 
 
-@app.route('/cars/<car_id>', methods=['GET', 'PUT', 'DELETE'])
-def handle_car(car_id):
-    car = CarsModel.query.get_or_404(car_id)
+# @app.route('/senors/<senors_id>', methods=['GET', 'PUT', 'DELETE'])
+# def handle_sensor(senors_id):
+#     sensor = Sensor.query.get_or_404(senors_id)
 
-    if request.method == 'GET':
-        response = {
-            "name": car.name,
-            "model": car.model,
-            "doors": car.doors
-        }
-        return {"message": "success", "car": response}
+#     if request.method == 'GET':
+#         response = {
+#             "name": sensor.name,
+#             "type": sensor.type,
+#             "temp": sensor.temp,
+#             "humi": sensor.humi,
+#             "date": sensor.date
+#         }
+#         return {"message": "success", "sensor": response}
 
-    elif request.method == 'PUT':
-        data = request.get_json()
-        car.name = data['name']
-        car.model = data['model']
-        car.doors = data['doors']
-        db.session.add(car)
-        db.session.commit()
-        return {"message": f"car {car.name} successfully updated"}
+#     elif request.method == 'PUT':
+#         data = request.get_json()
+#         if data['type'] == "ds18b20":
+#             sensor.name = data['name']
+#             sensor.temp = data['temp']
+#             sensor.date = get_timestamp_now()
+#         elif data['type'] == "si7021":
+#             sensor.name = data['name']
+#             sensor.temp = data['temp']
+#             sensor.humi = data['humi']
+#             sensor.date = get_timestamp_now()
 
-    elif request.method == 'DELETE':
-        db.session.delete(car)
-        db.session.commit()
-        return {"message": f"Car {car.name} successfully deleted."}
+#         db.session.add(sensor)
+#         db.session.commit()
+#         return {"message": f"sensor {sensor.name} successfully updated"}
+
+#     elif request.method == 'DELETE':
+#         db.session.delete(sensor)
+#         db.session.commit()
+#         return {"message": f"sensor {sensor.name} successfully deleted."}
+
+
+# @app.route('/cars', methods=['POST', 'GET'])
+# def handle_cars():
+#     if request.method == 'POST':
+#         if request.is_json:
+#             data = request.get_json()
+#             new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
+#             db.session.add(new_car)
+#             db.session.commit()
+#             return {"message": f"car {new_car.name} has been created successfully."}
+#         else:
+#             return {"error": "The request payload is not in JSON format"}
+
+#     elif request.method == 'GET':
+#         all_cars = CarsModel.query.all()
+#         # results = [
+#         #     {
+#         #         "name": car.name,
+#         #         "model": car.model,
+#         #         "doors": car.doors
+#         #     } for car in cars]
+
+#         return jsonify(cars_schema.dump(all_cars))
+
+
+# @app.route('/cars/<car_id>', methods=['GET', 'PUT', 'DELETE'])
+# def handle_car(car_id):
+#     car = CarsModel.query.get_or_404(car_id)
+
+#     if request.method == 'GET':
+#         response = {
+#             "name": car.name,
+#             "model": car.model,
+#             "doors": car.doors
+#         }
+#         return {"message": "success", "car": response}
+
+#     elif request.method == 'PUT':
+#         data = request.get_json()
+#         car.name = data['name']
+#         car.model = data['model']
+#         car.doors = data['doors']
+#         db.session.add(car)
+#         db.session.commit()
+#         return {"message": f"car {car.name} successfully updated"}
+
+#     elif request.method == 'DELETE':
+#         db.session.delete(car)
+#         db.session.commit()
+#         return {"message": f"Car {car.name} successfully deleted."}
 
 @app.route('/logging')
 def default_route():
@@ -348,8 +368,8 @@ def default_route():
     #app.run(host="0.0.0.0",port=5001, debug=True)
 
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
      
         
         
