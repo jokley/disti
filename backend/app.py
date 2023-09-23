e5e929851bcb23e504b634253ecd0e311f680e69
jokley <reinhard_juen@vol.at>
2023-09-23 21:10:46 +0200

    app

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,4 @@
 from flask import Flask, json, render_template, request,jsonify
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_marshmallow import Marshmallow
-# from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from datetime import datetime
 import pytz
 import psycopg2
@@ -14,10 +11,6 @@ import os
 
 load_dotenv()
 
-
-#TIMESTAMP_NOW = datetime.now().astimezone(pytz.timezone("Europe/Berlin")).isoformat()
-#TIMESTAMP_NOW_OFFSET = pytz.timezone("Europe/Berlin").utcoffset(datetime.now()).total_seconds()
-#TIMESTAMP_NOW_EPOCHE = int(datetime.now().timestamp()+TIMESTAMP_NOW_OFFSET)  
 
 def get_timestamp_now():
     TIMESTAMP_NOW = datetime.now().astimezone(pytz.timezone("Europe/Berlin")).isoformat()
@@ -43,8 +36,6 @@ def get_db_connection():
                             password=os.getenv("DOCKER_POSTGRES_INIT_PASSWORD"))
     return conn
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@postgres/flasksql'
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config['MQTT_BROKER_URL'] = "172.16.238.12"
 app.config['MQTT_BROKER_PORT'] = 1883
@@ -55,84 +46,11 @@ app.config['MQTT_CLIENT_ID']= 'jokley_flask_mqtt'
 
 app.secret_key = 'hi'
 
-# db = SQLAlchemy(app)
-# ma = Marshmallow(app)
 mqtt = Mqtt(app)
 
 
-
-# class Sensor(db.Model):
-#     id = db.Column(db.Integer)
-#     name = db.Column(db.Text(), nullable=False)
-#     type = db.Column(db.Text(), nullable=False)
-#     temp = db.Column(db.Float(2), nullable=True)
-#     humi = db.Column(db.Float(2), nullable=True)
-#     date = db.Column(db.DateTime(), nullable=False)
-
-#     # def __init__(self, name, type,temp,humi=None):
-#     #     self.name = name
-#     #     self.type = type
-#     #     self.temp = temp
-#     #     self.humi = humi
-    
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-       
-
-# class SensorSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Sensor
-#         ordered = True
-
-# sensor_schema = SensorSchema()
-# sensors_schema = SensorSchema(many=True)
-    
-
-# class People(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     pname = db.Column(db.String(80), unique=True, nullable=False)
-#     color = db.Column(db.String(120), nullable=False)
-    
-
-#     def __init__(self, pname, color):
-#         self.pname = pname
-#         self.color = color
-
-# class PeopleSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = People
-#         ordered = True
-
-# people_schema = PeopleSchema()
-# poeples_schema = PeopleSchema(many=True)
-
-# class CarsModel(db.Model):
-#     __tablename__ = 'cars'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String())
-#     model = db.Column(db.String())
-#     doors = db.Column(db.Integer())
-
-#     def __init__(self, name, model, doors):
-#         self.name = name
-#         self.model = model
-#         self.doors = doors
-
-# class CarSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = CarsModel
-#         ordered = True
-
-# car_schema = CarSchema()
-# cars_schema = CarSchema(many=True)
-
-
 mqtt.subscribe("sensors/#")
-# mqtt.subscribe("sensors/#")
-# mqtt.subscribe("sensors/#")
-
-    
+  
 @mqtt.on_message()
 def handle_message(client, userdata, message):
 #    if message.topic == "sensors/#":
@@ -158,23 +76,6 @@ def handle_message(client, userdata, message):
     cur.close()
     conn.close()
 
-# @mqtt.on_message()
-# def handle_message(client, userdata, message):
-#     # text = message.topic
-#     # x = text.split("/") 
-#     app.logger.info(message.topic)
-#     # data = json.loads(message.payload.decode())
-#     # app.logger.info({},{},{},{}).format(name=x[2], type=x[3], temp=data['temp'])
-#     # if x[0] == "sensors":
-#     #     #print(message.payload.decode())
-#     #     data = json.loads(message.payload.decode())
-#     #     if x[3] == "ds18b20":
-#     #         new_sensor =  Sensor(name="{}".format(x[2]), type="{}".format(x[3]), temp=data['temp'],date=get_timestamp_now()) 
-#     #     elif x[3]== "si7021":
-#     #         new_sensor =  Sensor(name="{}".format(x[2]), type="{}".format(x[3]), temp=data['temp'],humi=data['humi'],date=get_timestamp_now())   
-#     #     with app.app_context():
-#     #         db.session.add(new_sensor)
-#     #         db.session.commit()
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
@@ -213,120 +114,27 @@ def time():
     return jsonify(get_timestamp_now_epoche(),get_timestamp_now(),get_timestamp_now_offset())
 
 
-@app.route('/mqtt_subscribe')
-def mqtt_subscribe():
-    mqtt.subscribe("sensors/#")
-    return jsonify("subscribe topic sensors/#")
-
- 
-@app.route("/sensor_test")
-def sensor_test():
-        sName = 'Outdoor'
-        sType ='si7021'
-        sTemp = 25
-        sHumi = 45
-        
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute('INSERT INTO sensor (name, type, temp, humi)'
-                    'VALUES (%s, %s, %s, %s)',
-                    (sName, sType, sTemp, sHumi))
-        conn.commit()
-        cur.close()
-        conn.close()
-        return jsonify('Insert')
-
-
-# @app.route("/addperson")
-# def addperson():
-#     return render_template("index.html")
-
-
-# @app.route("/personadd", methods=['POST'])
-# def personadd():
-#     pname = request.form["pname"]
-#     color = request.form["color"]
-#     entry = People(pname, color)
-#     db.session.add(entry)
-#     db.session.commit()
-
-#     return render_template("index.html")
-
-
-# @app.route('/sensors', methods=['POST', 'GET'])
-# def handle_senors():
-#     if request.method == 'POST':
-#         if request.is_json:
-#             data = request.get_json()
-#             if data['type'] == "ds18b20":
-#                 new_sensor =  Sensor(name=data['name'],type=data['type'], temp=data['temp'],date=get_timestamp_now()) 
-#             elif data['type'] == "si7021":
-#                 new_sensor =  Sensor(name=data['name'],type=data['type'],temp=data['temp'],humi=data['humi'],date=get_timestamp_now())   
-
-#             db.session.add(new_sensor)
-#             db.session.commit()
-#             return {"message": f"sensor {new_sensor.name} has been created successfully."}
-#         else:
-#             return {"error": "The request payload is not in JSON format"}
-
-#     elif request.method == 'GET':
-#         FROM =request.args.get('from', default = get_timestamp_now_epoche()-36000, type = int)
-#         TO = request.args.get('to', default = get_timestamp_now_epoche(), type = int)
-
-#         VON = datetime.fromtimestamp(int(FROM + get_timestamp_now_offset())).isoformat()
-#         BIS = datetime.fromtimestamp(int(TO + get_timestamp_now_offset())).isoformat()  
-
-#         all_senors = Sensor.query.filter(Sensor.date >= VON).filter(Sensor.date<=BIS).order_by(Sensor.date).all()
-#     return jsonify(sensors_schema.dump(all_senors))
-
-
 @app.route('/sensors', methods=['GET'])
 def handle_sensor():
         conn = get_db_connection()
-        #cur = conn.cursor()
         cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-        cur.execute('SELECT * FROM sensor;')
+        cur.execute('SELECT * FROM sensor order by date;')
         sensors = cur.fetchall()
         cur.close()
         conn.close()
         return jsonify(sensors)
+
+
+@app.route('/cars', methods=['GET'])
+def handle_sensor():
+        conn = get_db_connection()
+        cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+        cur.execute('SELECT * FROM cars;')
+        cars = cur.fetchall()
+        cur.close()
+        conn.close()
+        return jsonify(cars)
     
-
-
-# @app.route('/senors/<senors_id>', methods=['GET', 'PUT', 'DELETE'])
-# def handle_sensor(senors_id):
-#     sensor = Sensor.query.get_or_404(senors_id)
-
-#     if request.method == 'GET':
-#         response = {
-#             "name": sensor.name,
-#             "type": sensor.type,
-#             "temp": sensor.temp,
-#             "humi": sensor.humi,
-#             "date": sensor.date
-#         }
-#         return {"message": "success", "sensor": response}
-
-#     elif request.method == 'PUT':
-#         data = request.get_json()
-#         if data['type'] == "ds18b20":
-#             sensor.name = data['name']
-#             sensor.temp = data['temp']
-#             sensor.date = get_timestamp_now()
-#         elif data['type'] == "si7021":
-#             sensor.name = data['name']
-#             sensor.temp = data['temp']
-#             sensor.humi = data['humi']
-#             sensor.date = get_timestamp_now()
-
-#         db.session.add(sensor)
-#         db.session.commit()
-#         return {"message": f"sensor {sensor.name} successfully updated"}
-
-#     elif request.method == 'DELETE':
-#         db.session.delete(sensor)
-#         db.session.commit()
-#         return {"message": f"sensor {sensor.name} successfully deleted."}
 
 
 # @app.route('/cars', methods=['POST', 'GET'])
@@ -378,23 +186,3 @@ def handle_sensor():
 #         db.session.delete(car)
 #         db.session.commit()
 #         return {"message": f"Car {car.name} successfully deleted."}
-
-@app.route('/logging')
-def default_route():
-    """Default route"""
-    app.logger.debug('this is a DEBUG message')
-    app.logger.info('this is an INFO message')
-    app.logger.warning('this is a WARNING message')
-    app.logger.error('this is an ERROR message')
-    app.logger.critical('this is a CRITICAL message')
-    return jsonify('hello world')
-    
-#if __name__ == "__main__":
-    #app.run(host="0.0.0.0",port=5001, debug=True)
-
-
-# with app.app_context():
-#     db.create_all()
-     
-        
-        
