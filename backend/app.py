fec4da5921b4baa3d1a2779f4ffda5019e86f1e0
jokley <reinhard_juen@vol.at>
2023-09-28 21:27:19 +0200

    flask blog

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, json, render_template, request,jsonify
+from flask import Flask, json,jsonify,render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
 from datetime import datetime
 import pytz
 import psycopg2
@@ -24,17 +25,31 @@ def get_timestamp_now_epoche():
     TIMESTAMP_NOW_EPOCHE = int(datetime.now().timestamp()+get_timestamp_now_offset())
     return TIMESTAMP_NOW_EPOCHE 
 
-
-app = Flask(__name__)
-CORS(app)
-
-
 def get_db_connection():
     conn = psycopg2.connect(host='postgres',
                             database='postgres',
                             user=os.getenv("DOCKER_POSTGRES_INIT_USERNAME"),
                             password=os.getenv("DOCKER_POSTGRES_INIT_PASSWORD"))
     return conn
+
+       
+def get_post(post_id):
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('SELECT * FROM posts WHERE id = %s;',(post_id,))
+    post = cur.fetchone()
+    cur.close()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
+
+
+
+app = Flask(__name__)
+CORS(app)
 
 
 app.config['MQTT_BROKER_URL'] = "172.16.238.12"
@@ -106,8 +121,78 @@ def handle_connect(client, userdata, flags, rc):
 
 
 @app.route('/')
-def home():
-    return jsonify('Dist API')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('SELECT * FROM posts;')
+    posts = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute('INSERT INTO posts (title, content) VALUES (%s, %s);',(title, content))
+            conn.commit()
+            cur.close()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute('UPDATE posts SET title = %s, content = %s WHERE id = %s;',(title, content, id))
+            conn.commit()
+            cur.close()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM posts WHERE id = %s;', (id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
+
+
+
 
 @app.route('/time')
 def time():
